b52979746a47aaf49c324a590536106b656b35d7
rewfe <qrewfe@yandex.ru>
2015-06-13 01:14:29 +0300

    Initial Commit

---
+++ Tutorial_31/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tutorial_31
+//
+//  Created by Admin on 13.06.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
